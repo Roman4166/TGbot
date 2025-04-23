c4a768e028224453e454e5b50be72b9de77b3515
Roman <trm.ru2006@gmail.com>
2025-04-23 23:37:06 +0300

    добавлена бд и проверка на повторный вход

--- Bot.py
+++ Bot.py
@@ -1,5 +1,6 @@
 from telebot import *
 from Token import *
+
 import sqlite3
 
 bot = telebot.TeleBot(TOKEN, parse_mode=None) 
@@ -22,39 +23,47 @@ connection.close()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+# Реагирует на команды старт и хелп и смотрит есть ли пользователь в базе если нет добавляет
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     name = message.from_user.username
     data = datetime.now().strftime("%d.%m.%Y")
-
-
+    
+    
     connection = sqlite3.connect('my_database.db')
     cursor = connection.cursor()
-    cursor.execute('INSERT INTO Users (username, data) VALUES (?, ?)', ( name , data ))
+    cursor.execute('SELECT username FROM Users WHERE  username = "{name}" '.format(name = name))
+    wer = cursor.fetchall()
+    for i in range(len(wer)):
+        if wer[i][0] == str(name):
+            print('старый пользователь')
+            break
+    else: 
+        cursor.execute('INSERT INTO Users (username, data) VALUES (?, ?)', ( name , data ))
+        print('успешно',name)
     connection.commit()
     connection.close()
-    
-    
-    bot.reply_to(message, f'{message.from_user.username}привет')
+    bot.reply_to(message, f'{message.from_user.username} привет')
     bot.send_message(message.from_user.id,text='привет')
 
+
+
+#Открывает сайт КазГАУ
 @bot.message_handler(commands=['command1'])
 def execute_the_command1(message):
-    print("command1")
+    bot.send_message(message.from_user.id,text="https://kazgau.ru/")
+
+
+
+
 @bot.message_handler(commands=['command2'])
 def execute_the_command2(message):
     print("command2")
+
+
+
+
+
 
 bot.infinity_polling()
 
